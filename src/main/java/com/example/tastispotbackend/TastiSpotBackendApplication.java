b193cba943078ae541d4eb204cc6df9fc9a4afbd
hunzzzzz <hun819.st@gmail.com>
2025-11-03 20:40:13 +0900

    프로젝트 초기 파일 업로드

---
+++ src/main/java/com/example/tastispotbackend/TastiSpotBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.tastispotbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TastiSpotBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TastiSpotBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tastispotbackend/TastiSpotBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tastispotbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TastiSpotBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
