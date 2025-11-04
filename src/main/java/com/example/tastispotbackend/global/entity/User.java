c8e261272fb4d6098ef86e2e74ef87ee0ebe926f
hunzzzzz <hun819.st@gmail.com>
2025-11-04 20:46:41 +0900

    user 엔티티 수정

--- src/main/java/com/example/tastispotbackend/global/entity/User.java
+++ src/main/java/com/example/tastispotbackend/global/entity/User.java
@@ -12,21 +12,25 @@ import java.util.UUID;
 @Setter
 public class User {
     private String id;
-    private String role;
+    private UserRole role;
     private String name;
     private String nickname;
     private String email;
     private String password;
+    private String rtk;
+    private ZonedDateTime lastLoginAt;
     private ZonedDateTime createdAt;
     private ZonedDateTime updatedAt;
 
     public User(SignupRequest request, BCryptPasswordEncoder passwordEncoder) {
         this.id = UUID.randomUUID().toString();
-        this.role = ""; // TODO
+        this.role = UserRole.USER;
         this.name = request.name();
         this.nickname = request.nickname();
         this.email = request.email();
         this.password = passwordEncoder.encode(request.password());
+        this.rtk = null;
+        this.lastLoginAt = null;
         this.createdAt = ZonedDateTime.now();
         this.updatedAt = ZonedDateTime.now();
     }
---
+++ src/main/java/com/example/tastispotbackend/global/entity/UserRole.java
@@ -0,0 +1,5 @@
+package com.example.tastispotbackend.global.entity;
+
+public enum UserRole {
+    USER, ADMIN
+}
